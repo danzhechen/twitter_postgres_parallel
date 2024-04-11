36914de582273a65c7decfeee479e1e4ccf248b0
danzhechen <chendanzhe@gmail.com>
2024-04-10 20:56:22 -0700

    batch test

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -9,9 +9,8 @@ time echo "$files" | parallel ./load_denormalized.sh
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-# FIXME: implement this with GNU parallel
-
+time echo "$files" | parallel python3 load_tweets.py --db "postgresql://postgres:pass@localhost:7399" --inputs
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-# FIXME: implement this with GNU parallel
+time echo "$files" | parallel python3 -u load_tweets_batch.py --db "postgresql://postgres:pass@localhost:8399" --inputs
